40ab2284822b6d79b12609a5f3a1a1eac0cba8f6
iamkapil <kapilrhngdle2211@gmail.com>
2024-01-09 16:35:12 +0530

    done

---
+++ frontend/src/Components/Authentication/Login.js
@@ -0,0 +1,146 @@
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Link, Stack, VStack, useToast } from '@chakra-ui/react';
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useHistory } from 'react-router-dom'
+import AuthSocial from './AuthSocial';
+import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
+
+
+
+const Login = () => {
+    // we use hook to manage state with a functional component
+    const [show, setShow] = useState(false);
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
+    const history = useHistory();
+
+    const handleClick = () => {
+        setShow(!show);
+        console.log(email);
+        console.log(password);
+    }
+    
+  //  a function (submitHandler) responsible for handling login form submissions,
+
+    const submitHandler = async () => {
+        // setloading help in indicating some process is ongoing 
+        setLoading(true);
+        // Input Field Validation:- If either field is empty, it triggers a toast notification using the toast function  to inform the user that they need to fill in all fields.
+        if (!email || !password) {
+            toast({
+                title: "Please Fill all the Feilds",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            //  showing the toast notification, it sets setLoading(false) and returns from the function, preventing further execution.
+            setLoading(false);
+            return;
+        }
+
+        // API Request to Backend:If both the email and password fields are filled, it proceeds to make an asynchronous request using Axios to a backend endpoint (/api/user/login) to attempt user login.
+
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+          // // It sends a POST request with the email and password in the request body and sets the content type as JSON.
+            const { data } = await axios.post(
+                '/api/user/login',
+                { email, password },
+                config
+            );
+
+            console.log(JSON.stringify(data));
+            // it show a success toast notification indicating successful logiin
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            // stores user info in local storage
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            //Navigates the user to the "/chats" route using useHistory hook
+            history.push("/chats");
+        }
+        // error handling 
+        catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                toast({
+                    title: "Error Occurred!",
+                    description: error.response.data.message,
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                    position: "bottom",
+                });
+            } else {
+                toast({
+                    title: "Error Occurred!",
+                    description: "An unexpected error occurred.",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                    position: "bottom",
+                });
+            }
+            setLoading(false);
+        }
+
+    }
+    return (
+        // <Stack>
+        //     New User?
+        // </Stack>
+        <VStack spacing='5px' >
+            
+            <FormControl id='loginEmail' isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} type='email' value={email} />
+            </FormControl>
+
+            <FormControl id='loginPassword' isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                    <Input placeholder='Enter Your Password' onChange={(e) => setPassword(e.target.value)} type={show ? 'text' : 'password'} value={password} />
+                    <InputRightElement width={'4.5rem'}>
+                        <Button fontSize={25} backgroundColor={'inherit'} onClick={handleClick}>
+                            {show ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+
+            <Button colorScheme='cyan' width={'100%'} style={{ marginTop: 15 }} onClick={submitHandler} isLoading={loading} textColor={'aliceblue'}>
+                Login
+            </Button>
+            <Stack
+                alignItems="flex-end"
+                sx={{ my: 2 }}>
+                {/* <Link
+                    variant="body2"
+                    color="inherit">
+                    Forgot Password?
+                </Link> */}
+            </Stack>
+            <AuthSocial />
+            {/* <Button variant={'solid'} colorScheme='red' width={'100%'} style={{ marginTop: 15 }} onClick={() => {
+                setEmail('guestexample@gmail.com')
+                setPassword('123456')
+            }}>
+                Get Guest User Credentials
+            </Button> */}
+        </VStack>
+    )
+}
+
+export default Login
---
+++ frontend/src/Components/Authentication/Signup.js
@@ -0,0 +1,183 @@
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack, useToast, } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useHistory } from 'react-router-dom'
+import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
+
+const Signup = () => {
+    const [show, setShow] = useState(false);
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [confirmPassword, setConfirmPassword] = useState()
+    const [phoneNumber, setPhoneNumber] = useState(0)
+    const [pic, setPic] = useState()
+    const [loading, setLoading] = useState(false);
+    const toast = useToast()
+    const history = useHistory()
+
+    const handleClick = () => {
+        setShow(!show);
+    }
+// a function which will trigger to handle when Upload image buutonis clicked
+    const postDetails = (pics) => {
+        setLoading(true);
+        // image validation
+        if(pics===undefined){
+            toast({
+                title: 'Please select an Image.',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position:'bottom',
+              })
+            return
+        }
+        // image upload to Cloudinary if the file is of the appropriate type
+
+        if(pics.type==='image/jpeg' || pics.type==='image/png'){
+            // it creates a FormData object and appends the image file and required parameters for Cloudinary upload
+            const data = new FormData()
+            data.append('file',pics);
+            data.append('upload_preset','chat-app');
+            data.append('cloud_name','ddevil');
+
+            // uses fetch to send a POST request to the Cloudinary API endpoint with the FormData as the body. 
+            fetch('https://api.cloudinary.com/v1_1/ddevil/image/upload',{
+                method:'post',
+                body:data,
+            }).then((res)=> res.json())// Upon a successful response from Cloudinary, it extracts the URL of the uploaded image from the response data (data.url), sets it using setPic
+              .then((data)=>{
+                setPic(data.url.toString());
+                console.log(data.url.toString());
+                setLoading(false);
+              })
+              .catch((err)=>{ // error handling
+                console.log(err);
+                setLoading(false)
+              })
+        }
+        else{
+            toast({
+                title: 'Please select an Image.',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position:'bottom',
+              })
+            setLoading(false)
+            return
+        }
+    }
+
+    const submitHandler = async ()=>{
+        setLoading(true);
+        if (!name || !email || !password || !confirmPassword) {
+            toast({
+                title: 'Please fill all the required (*) field.',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            setLoading(false)
+            return
+        }
+
+        if (password !== confirmPassword) {
+            toast({
+                title: 'Please fill all the required (*) field.',
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            return
+        }
+
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                }
+            }
+            const { data } = await axios.post('/api/user', { name, email, password, pic }, config)
+            toast({
+                title: 'Registration Successful.',
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom',
+            })
+
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history.push('/chats')
+            history.go(0);
+
+        } catch (error) {
+            toast({
+                title: 'Error Occured.',
+                description: error.reponse.data.message,
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom',
+            })
+            setLoading(false);
+        }
+    }
+    return (
+        <VStack spacing='5px'>
+            <FormControl id='first-name' isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input placeholder='Enter Your Name' onChange={(e) => setName(e.target.value)} />
+            </FormControl>
+            <FormControl id='email' isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} type='email' />
+            </FormControl>
+
+            <FormControl id='password' isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                    <Input placeholder='Enter Your Password' onChange={(e) => setPassword(e.target.value)} type={show ? 'text' : 'password'} />
+                    <InputRightElement width={'4.5rem'}>
+                        <Button fontSize={25} backgroundColor={'inherit'} onClick={handleClick} >
+                            {show ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+
+            <FormControl id='confirm-password' isRequired>
+                <FormLabel>Confirm Password</FormLabel>
+                <InputGroup>
+                    <Input placeholder='Enter Your Confirm Password' onChange={(e) => setConfirmPassword(e.target.value)} type={show ? 'text' : 'password'} />
+                    <InputRightElement width={'4.5rem'}>
+                        <Button fontSize={25} backgroundColor={'inherit'} onClick={handleClick} >
+                            {show ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+
+            <FormControl id='phone-number' >
+                <FormLabel>Phone Number</FormLabel>
+                <Input placeholder='Enter Your Phone Number' onChange={(e) => setPhoneNumber(e.target.value)} maxLength={10} />
+            </FormControl>
+
+            <FormControl id='pic'>
+                <FormLabel>Upload Your Image Here</FormLabel>
+                <Input type='file' p={1.5} accept='image/*' onChange={(e) => postDetails(e.target.files[0])} />
+            </FormControl>
+
+            <Button colorScheme='cyan' width={'100%'} style={{ marginTop: 15 }} onClick={submitHandler} isLoading={loading} textColor={'aliceblue'}>
+                Sign Up
+            </Button>
+        </VStack>
+    )
+}
+
+export default Signup
